1635ca701d8d6cd1f6bca02e523ff40c058e8481
Talia Harrison-Marcassa <hm.talia@gmail.com>
2017-03-13 14:56:37 -0400

    components

--- public/app.js
+++ public/app.js
@@ -20489,6 +20489,8 @@ module.exports = require('./lib/React');
 },{"./lib/React":155}],178:[function(require,module,exports){
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -20499,10 +20501,65 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_reactDom2.default.render(_react2.default.createElement(
-  'h1',
-  null,
-  'Hello World'
-), document.getElementById('app'));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var App = function (_React$Component) {
+	_inherits(App, _React$Component);
+
+	function App() {
+		_classCallCheck(this, App);
+
+		return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+	}
+
+	_createClass(App, [{
+		key: 'render',
+		value: function render() {
+			return _react2.default.createElement(
+				'div',
+				null,
+				_react2.default.createElement(
+					'h1',
+					null,
+					'Hello!'
+				),
+				_react2.default.createElement(
+					'p',
+					null,
+					'A tag line for my div'
+				),
+				_react2.default.createElement(
+					'ul',
+					null,
+					_react2.default.createElement(
+						'li',
+						null,
+						'A list'
+					),
+					_react2.default.createElement(
+						'li',
+						null,
+						'of items!'
+					),
+					_react2.default.createElement(
+						'li',
+						null,
+						'And more items!'
+					)
+				)
+			);
+		}
+	}]);
+
+	return App;
+}(_react2.default.Component);
+
+_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById('app'));
+
+//usually you have one main component, only have render method once!
 
 },{"react":177,"react-dom":26}]},{},[178]);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(<h1>Hello World</h1>, document.getElementById('app'));
+
+class Searchbar extends React.Component {
+	
+}
+
+
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello!</h1>	
+				<p>A tag line for my div</p>
+				<ul>
+					<li>A list</li>
+					<li>of items!</li>
+					<li>And more items!</li>
+				</ul>
+			</div>
+		)
+	}
+}
+
+
+
+
+
+
+ReactDom.render(<App />, document.getElementById('app'));
+
+//usually you have one main component, only have render method once!
 
